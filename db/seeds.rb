f0d5b325130a73fa846986e9af597e2fda0c45c0
wesf90 <wesfed@gmail.com>
2015-06-23 18:10:12 -0500

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,27 +15,49 @@ user = User.create!(
 	confirmed_at: 					Date.yesterday
 )
 
-business = Business.create!(
-	user_id:   user.id,
-	name:      "My business",
-	published: true,
-	primary_color: 'fff',
-	secondary_color: '000',
-	twitter: 'wer'
+business1 = Business.create!(
+	user_id:   				user.id,
+	name:      				"My business",
+	published: 				true,
+	primary_color: 		'ffffff',
+	secondary_color: 	'000000',
+	website: 					'http://getthatyummycoffee.com',
+	facebook: 				'fbyummy',
+	twitter: 					'GetCoffee'
+)
+
+greeting = Greeting.create(
+	welcome_message: 			"Welcome!",
+	welcome_reward: 			100,
+	welcome_reward_freq: 	0, #Daily
+	exit_message: 				"Goodbye!",
+	exit_campaign_id:			1,
+	exit_freq_days:				3,
+	exit_freq_type:				0 # 0=Days
 )
 
 location = Location.create!(
 	business_id: 	business.id,
-	beacon_id: 		nil,
+	greeting_id: 	greeting.id,
 	title: 				"My location",
-	address: 			"123 Street",
+	address: 			"100 S Hampton Pl",
 	address2: 		"",
 	city: 				"Clarksville",
 	state: 				"TN",
-	zipcode: 			"37040"
+	zipcode: 			"37040",
+	latitude: 		36.5896212,
+	longitude: 		-87.2933637
 )
 
-receipts = Receipt.create!(
-	location_id: location.id,
-	image_filename: "receipt.jpg"
+beacon = Beacon.create!(
+	location_id: 	location.id,
+	uuid: 				"1234567890",
+	void: 				false
+)
+
+customer = Customer.create!(
+	first_name: 	"Jack",
+	last_name: 		"Johnson",
+	email: 				"customer@gmail.com",
+	active: 			false
 )
