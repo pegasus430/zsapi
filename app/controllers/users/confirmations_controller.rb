688347eb58b3daf6da622431d6143ef3a58a8a22
wesf90 <wesfed@gmail.com>
2015-10-08 17:56:02 -0500

    uncommented the flash when confirmation is done

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -3,8 +3,8 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   private
 
   	def after_confirmation_path_for(resource_name, resource)
-  		# flash[:success] = "Your account has been confirmed! Continue by creating your business."
-    	new_business_path
+  		flash[:success] = "Your account has been confirmed! Continue by creating your business."
+    	edit_business_path
   	end
 
 end
