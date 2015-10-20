712d5aacb3d62a3f40a1e4e74b1cf691612f4186
wesf90 <wesfed@gmail.com>
2015-10-20 10:36:33 -0500

    updated colors in scss

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,13 +1,7 @@
 module CampaignsHelper
 
 	def type_of_circle(type_of)
-		color = case type_of
-		when 'coupon' 	then 'green'
-		when 'reward' 	then 'blue'
-		when 'special' 	then 'red'
-		end
-
-		raw "<span class='circle #{color}'>#{type_of[0].upcase}</span>"
+		raw "<span class='circle #{type_of}'>#{type_of[0].upcase}</span>"
 	end
 
 end
