27455eba6286f7149a05993b271dc3363b1d0610
wesf90 <wesfed@gmail.com>
2015-10-21 18:05:12 -0500

    updaing components and removing unnecessary scss

--- app/views/HTMLDev/js/plugins/js-cookies/main.js
+++
@@ -1,7 +0,0 @@
-    $(document).ready(function() {
-        /*if ($.cookie('profile-progress') == 'yes') {
-          $('.profile-progress').addClass('hide-content');
-        }*/
-
-        $.cookie('profile-progress', 'yes', {expires: 7 });
-    });
