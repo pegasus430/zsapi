178b7b6f61d3aaa14de69ad2c57cc76118215241
wesf90 <wesfed@gmail.com>
2015-11-20 23:20:31 -0600

    Updates datePickable to ensure that forms parse datepicker dates correctly.

--- app/models/concerns/date_pickable.rb
+++ app/models/concerns/date_pickable.rb
@@ -8,6 +8,8 @@ module DatePickable
 		def datepicker(attributes=[])
 			if attributes.is_a? Array
 				attributes.each do |a|
+
+					# attribute=ASSIGN => Convert from MM/DD/YYYY to YYYY-MM-DD
 					define_method("#{a}=") do |string_date|
 						unless string_date.blank?
 							parsed_date = case string_date
@@ -20,6 +22,12 @@ module DatePickable
 				    	super string_date
 				    end
 			    end
+
+			    # attribute => MM/DD/YYYY
+					define_method("#{a}") do
+						self[a.to_sym].strftime("%m/%d/%Y") rescue ""
+					end
+			    
 			  end
 			end
 		end
