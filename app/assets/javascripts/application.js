9b5050f728668fe76ca62f8cd59d46166c6c14ad
Isaac Gregson <isaac@isaacgregson.com>
2015-12-01 19:08:10 +0300

    fixes navbar/body spacing bug on mobile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,5 +31,8 @@ Notes:
 var globalInitializers = require('./global/global.inits')
 globalInitializers()
 
+var sidebarInit = require('./global/global.sidebar')
+sidebarInit()
+
 var globalModals = require('./global/global.modals')
 globalModals()
--- app/assets/javascripts/global/global.inits.js
+++ app/assets/javascripts/global/global.inits.js
@@ -17,35 +17,6 @@ module.exports = function() {
 
 
 
-
-  // Navbar Collapsing
-
-  // $(".nav-trigger").click(function() {
-  $("#navTrigger").click(function() {
-    $("body").toggleClass("sb-is-open")
-    $.get('/users/toggleSidebar')
-  })
-
-
-
-
-
-  // Swipe to open menu
-
-  if ( $(!".minicolors").length > 0 )
-  {
-    $('body').on("swipeleft",function(){
-      $(this).removeClass("sb-is-open")
-    })
-    $('body').on("swiperight",function(){
-      $(this).addClass("sb-is-open")
-    })
-  }
-
-
-
-
-
   // Tooltip menus
   $('[data-toggle="tooltip"]').tooltip({html: true})
 
