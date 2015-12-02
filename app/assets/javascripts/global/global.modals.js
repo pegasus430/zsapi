d5294ae4e7551e12a3ced69e760e62048abebe65
Isaac Gregson <isaac@isaacgregson.com>
2015-12-02 13:03:10 +0300

    completes gallery layout (via Masonry) and adds image hovers

--- app/assets/javascripts/global/global.modals.js
+++ app/assets/javascripts/global/global.modals.js
@@ -1,34 +1,71 @@
 
+var Masonry = require('masonry-layout')
+var imagesLoaded = require('imagesloaded')
 
 
-// TODO: consider refacoring this into css... 
+
 
 
 module.exports = function() {
 
-  "use strict";
-
-  function centerModal() {
-
-    $(this).css('display', 'block');
-
-    var $dialog      = $(this).find(".modal-dialog");
-    var offset       = ($(window).height() - $dialog.height()) / 2;
-    var bottomMargin = parseInt($dialog.css('marginBottom'), 10);
-
-    // Make sure you don't hide the top part of the modal w/ a negative margin if it's longer than the screen height, and keep the margin equal to the bottom margin of the modal
-    if(offset < bottomMargin) {
-      offset = bottomMargin;
-    }
-
-    $dialog.css("margin-top", offset);
-    
-  }
+  // "use strict";
 
   $(document).on('show.bs.modal', '.modal', centerModal);
+  
+  $(document).on('show.bs.modal', '.modal-gallery', modalGallery);
 
   $(window).on("resize", function () {
     $('.modal:visible').each(centerModal);
   });
+
+}
+
+
+
+
+
+
+
+/*
+
+Helper scripts for this module
+
+Notes:
+- if any of these need to be used in other modules, move them
+  into a helper file (to then be imported and used here)
+*/
+
+
+
+function centerModal() {
+
+  $(this).css('display', 'block');
+
+  var $dialog      = $(this).find(".modal-dialog");
+  var offset       = ($(window).height() - $dialog.height()) / 2;
+  var bottomMargin = parseInt($dialog.css('marginBottom'), 10);
+
+  // Make sure you don't hide the top part of the modal w/ a negative margin if it's longer than the screen height, and keep the margin equal to the bottom margin of the modal
+  if(offset < bottomMargin) {
+    offset = bottomMargin;
+  }
+
+  $dialog.css("margin-top", offset);
+  
+}
+
+
+function modalGallery() {
+  
+  var isoContainer = document.querySelector('.modal-gallery_container');
+
+  imagesLoaded( isoContainer, function() {
+
+    var msnry = new Masonry( isoContainer, {
+      itemSelector: '.modal-gallery_item',
+      columnWidth: '#modalGallerySizer'
+    });
+
+  })  
 
 }
