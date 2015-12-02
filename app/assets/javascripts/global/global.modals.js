1acd8daa61ea9978cd32449d7b68d29316b30f4b
Isaac Gregson <isaac@isaacgregson.com>
2015-12-02 13:29:54 +0300

    improves modal script file - adds comments, spaces things out

--- app/assets/javascripts/global/global.modals.js
+++ app/assets/javascripts/global/global.modals.js
@@ -65,23 +65,31 @@ function modalGallery() {
     var msnry = new Masonry( isoContainer, {
       itemSelector: '.modal-gallery_item',
       columnWidth: '#modalGallerySizer'
-    });
+    })
 
   })
+
 
   var modalGalleryItems = $('.modal-gallery_item')
   var selectedCount = 0
   var btn_confirmSelection = $('#btnGalleryConfirm')
 
+
   modalGalleryItems.each(function() {
     $(this).on('click', function() {
       $(this).toggleClass('selected')
       
+      
+      // Increment/Decrement selected count
+
       if ( $(this).hasClass('selected') ) {
         selectedCount += 1
       } else {
         selectedCount -= 1
       }
+
+
+      // Enable/Disable confirm btn
 
       if ( selectedCount > 0 ) {
         btn_confirmSelection.removeClass('btn-disabled')
