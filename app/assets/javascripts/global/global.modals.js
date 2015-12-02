ebe27b08d05d75874b31dd0ecc4ab0045e65551a
Isaac Gregson <isaac@isaacgregson.com>
2015-12-02 09:58:03 +0300

    fixes modal center sript syntax

--- app/assets/javascripts/global/global.modals.js
+++ app/assets/javascripts/global/global.modals.js
@@ -4,40 +4,31 @@
 // TODO: consider refacoring this into css... 
 
 
-
 module.exports = function() {
+
   "use strict";
 
   function centerModal() {
 
-
     $(this).css('display', 'block');
 
+    var $dialog      = $(this).find(".modal-dialog");
+    var offset       = ($(window).height() - $dialog.height()) / 2;
+    var bottomMargin = parseInt($dialog.css('marginBottom'), 10);
 
-    var $dialog      = $(this).find(".modal-dialog")
-    var offset       = ($(window).height() - $dialog.height()) / 2
-    var bottomMargin = parseInt($dialog.css('marginBottom'), 10)
+    // Make sure you don't hide the top part of the modal w/ a negative margin if it's longer than the screen height, and keep the margin equal to the bottom margin of the modal
+    if(offset < bottomMargin) {
+      offset = bottomMargin;
+    }
 
-
-    // Make sure you don't hide the top part of the modal 
-    // w/ a negative margin if it's longer than the screen 
-    // height, and keep the margin equal to the bottom margin of the modal
-
-    if ( offset < bottomMargin ) {
-
-      $dialog.css("margin-top", bottomMargin)
-
-    } 
-
-
-    $(document).on('show.bs.modal', '.modal', centerModal)
-
-    $(window).on("resize", function () {
-
-      $('.modal:visible').each(centerModal)
-
-    })
-
+    $dialog.css("margin-top", offset);
+    
   }
+
+  $(document).on('show.bs.modal', '.modal', centerModal);
+
+  $(window).on("resize", function () {
+    $('.modal:visible').each(centerModal);
+  });
 
 }
