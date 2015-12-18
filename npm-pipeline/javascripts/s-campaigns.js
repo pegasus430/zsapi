c74ac4e28b1abf735a2329262a6b44eb36dd488b
Isaac Gregson <isaac@isaacgregson.com>
2015-12-18 20:18:30 +0300

    Incorporates into campaign index

--- npm-pipeline/javascripts/s-campaigns.js
+++ npm-pipeline/javascripts/s-campaigns.js
@@ -4,6 +4,8 @@ modalGallery()
 
 
 
+// Listen for changes on the location filter dropdown
+// and redirect the window appropriately
 
 var locationFilter = require('./components/location-filter') 
 locationFilter()
