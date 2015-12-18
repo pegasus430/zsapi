901ed1b6a3180c3e5cc52810dc2c213f4df79aa5
Isaac Gregson <isaac@isaacgregson.com>
2015-12-19 00:05:48 +0300

    Fixes locations dropdown on Dashboard (adds JS).

---
+++ npm-pipeline/javascripts/s-pages.js
@@ -0,0 +1,19 @@
+/*
+
+s-pages
+
+
+Notes:
+- a per-controller-specific file
+- included on the Dashboard
+
+*/
+
+
+
+
+// Listen for changes on the location filter dropdown
+// and redirect the window appropriately
+
+var locationFilter = require('./components/location-filter') 
+locationFilter()
