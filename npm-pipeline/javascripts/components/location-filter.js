b1fa5387b4815c0e9f2dc285cd850b19a9deae38
Isaac Gregson <isaac@isaacgregson.com>
2015-12-18 20:42:23 +0300

    Account for selectin of "+ New Location"

--- npm-pipeline/javascripts/components/location-filter.js
+++ npm-pipeline/javascripts/components/location-filter.js
@@ -5,15 +5,17 @@ Location Filter Logic (dropdowns)
 Notes: 
 
 - To activate, add to HTML:
-
 1.  id="locationFilter" 
     to an element wrapping the select dropdown 
-
 2.  data-url-pattern="/locations/id/campaigns/<%= @campaign[:id] %>"
     where the value of the data-url-pattern attribute is the desired
     url pattern. 
     IMPORTANT: "id" in the value will be dynamically replaced with 
     the selected value's id
+- There's a plugin being used for the selectboxes that necessitates
+  a call to their method for the change event to work
+  See more on this on their Github readme: 
+  https://github.com/marcj/jquery-selectBox
 
 */
 
@@ -34,9 +36,14 @@ module.exports = function() {
 
 updateUrl = function() {
 
+  // The core div with the necessary id and data attributes
+
+  var filterWrap = $('#locationFilter')
+  
+
   // Get the url pattern
 
-  var urlPattern = $('#locationFilter').data('url-pattern')
+  var urlPattern = filterWrap.data('url-pattern')
 
 
   // Listen for location selections
@@ -48,15 +55,22 @@ updateUrl = function() {
 
       var targetID = $(this).val()
     
+      // Account for selection of "+ New Location"
 
-      // Replace the 'id' in teh urlPattern with the 
-      // selected location's id, 
-      // Leving us with the url to go to
+      if ( targetID === '0' ) {
+      
+        var goToURL = filterWrap.data('new-location')
 
-      var stringToReplace = /id/i
-      var goToURL = urlPattern.replace( stringToReplace, targetID )
+      } else {
 
-      console.log(urlPattern)
+        // Replace the 'id' in teh urlPattern with the 
+        // selected location's id, 
+        // Leving us with the url to go to
+
+        var stringToReplace = /id/i
+        var goToURL = urlPattern.replace( stringToReplace, targetID )
+      }
+
       console.log(goToURL)
 
       
