e8acf6f388e9ecd39e32c934f3dd3590610a148c
wesf90 <wesfed@gmail.com>
2015-12-31 17:37:44 -0600

    Creates the kontaktio API service object

---
+++ app/services/kontaktio.rb
@@ -0,0 +1,60 @@
+class Kontaktio
+	require 'excon'
+
+	attr_accessor :response
+
+	def initialize(api_key)
+		@api_key = api_key
+	end
+
+	def devices(query="")
+		get "/device?#{query}"
+		response_to_json
+	end
+
+	def beacon(query="")
+		get "/beacon?#{query}"
+		response_to_json
+	end
+
+
+	private
+
+		attr_accessor :api_key
+
+		def get(path="")
+			send_curl("get", path)
+		end
+
+		def post(path="", post_vars={})
+			send_curl("post", path, post_vars)
+		end
+
+		def send_curl(method, path="", post_vars={})
+			url = "https://api.kontakt.io#{path}"
+			
+			headers = {
+				'Content-Type' => "application/x-www-form-urlencoded",
+				'Accept'       => "application/vnd.com.kontakt+json;version=5",
+				'Api-Key'      => @api_key
+			}
+
+			connection = Excon.new(url)
+
+			if method == "get"
+				@response = connection.get(headers: headers)
+			elsif method == "post"
+				@response = connection.post(
+					headers: headers,
+					body: URI.encode_www_form(post_vars)
+				)
+			end
+
+			@response
+		end
+
+		def response_to_json
+			JSON.parse(@response.body)
+		end
+
+end
