6a5f272b0d66f37067bced6d9a88dc97cb7d1bcb
Isaac Gregson <isaac@isaacgregson.com>
2016-01-18 18:15:13 +0300

    Further removes (remaining) menu-swiping related code.

--- npm-pipeline/javascripts/global/global.sidebar.js
+++ npm-pipeline/javascripts/global/global.sidebar.js
@@ -5,12 +5,7 @@ Globals - Sidebar
 */
 
 
-var swipe = require('jquery-touchswipe')
-
-
 module.exports = function() {
-
-
 
   // Navbar Toggle
 
@@ -19,44 +14,5 @@ module.exports = function() {
     $("body").toggleClass("sb-is-open")
     $.get('/users/toggleSidebar')
   })
-
-
-
-
-  // Swipe menu implementation causing no-scroll bug, 
-  // due to bug with jquery-touchswipe
-  // commenting out for now, as menu toggle is fully
-  // functional via the toggle button
-
-  // Swipe Toggle
-
-  // $(function() {
-  //   $(".swipe-area").swipe( {
-  //     swipe:function(event, direction) {
-  //       // Open on swipe right
-  //       if ( direction === 'right' ) {
-  //         $("body").addClass('sb-is-open')
-  //       }
-  //     },
-  //     preventDefaultEvents: false
-  //   })
-
-  // });
-
-  // $(function() {
-  //   $("body").swipe({
-  //     swipe:function(event, direction) {
-  //       if ( direction === 'left' ) {
-  //         // Close on swipe left
-  //         $("body").removeClass('sb-is-open')
-  //       }
-  //     },
-  //     preventDefaultEvents: false
-  //   })
-  // })
-
-
-
-
 
 }
