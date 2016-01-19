3030972d3eedbfdd8170e98effeae8ac066ef398
Isaac Gregson <isaac@isaacgregson.com>
2016-01-19 11:42:55 +0300

    Fixes layout/spacing bug for business integrations.

--- app/helpers/businesses_helper.rb
+++ app/helpers/businesses_helper.rb
@@ -2,7 +2,7 @@ module BusinessesHelper
 	
 	def connect_identity_link(provider)
 		if current_user.send("#{provider}").nil?
-			link_to "Connect to #{provider}", user_omniauth_upgrade_path( provider ), class: "btn btn-default connect"
+			link_to "Connect", user_omniauth_upgrade_path( provider ), class: "btn btn-default connect"
 		else
 			content_tag :div, raw("Connected<br>(" + current_user.send(provider).name + ")"), class: "btn connected"
 		end
