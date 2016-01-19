7d73be9bab7406a6e0c128f671e47802bd093b78
Isaac Gregson <isaac@isaacgregson.com>
2016-01-19 19:00:19 +0300

    Fixes hover bug on business integration buttons.

--- npm-pipeline/javascripts/global/global.sidebar.js
+++ npm-pipeline/javascripts/global/global.sidebar.js
@@ -9,7 +9,6 @@ module.exports = function() {
 
   // Navbar Toggle
 
-  // $(".nav-trigger").click(function() {
   $("#navTrigger").click(function() {
     $("body").toggleClass("sb-is-open")
     $.get('/users/toggleSidebar')
