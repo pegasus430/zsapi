1410206470de9756c866edd359d6550d00d6f6bd
wesf90 <wesfed@gmail.com>
2016-01-20 14:48:25 -0600

    Limits the dashboard campaigns to 4

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
   
   def dashboard
   	@locations = current_user.business.locations rescue nil
-  	@campaigns = current_user.business.campaigns rescue nil
+  	@campaigns = current_user.business.campaigns.limit(4) rescue nil
   	@todays_campaigns = @campaigns.valid_for(Date.today)
 
   	# Stats
